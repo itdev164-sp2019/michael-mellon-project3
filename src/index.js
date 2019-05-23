d86bbe8a55034446ae796e51ee239024f2fc7440
Michael Mellon <mellonm@gmatc.matc.edu>
2019-05-23 16:45:20 -0500

    PRoject changed - blah

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import Typography from "@material-ui/core/Typography";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+import useTodoState from "./components/useTodoState";
+import "./style.scss";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+  const { todos, addTodo, deleteTodo } = useTodoState([]);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  return (
+    <div className="App">
+      <br />
+      <Typography component="h1" variant="h2">
+        Quick-Do
+        <br />
+        <br />
+      </Typography>
+      <TodoForm
+        saveTodo={todoText => {
+          const trimmedText = todoText.trim();
+
+          if (trimmedText.length > 0) {
+            addTodo(trimmedText);
+          }
+        }}
+      />
+      <TodoList todos={todos} deleteTodo={deleteTodo} />
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(<App />, rootElement);
